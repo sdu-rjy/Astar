bcb43c2e0a42e8a466cf58e85e7d118aa9d01cb0
sdu_rjy <sdu_rjy@163.com>
2021-01-09 16:06:34 +0800

    ENH:增加cmakelists文件

---
+++ build/CMakeFiles/Astar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Astar.dir/main.o"
+  "CMakeFiles/Astar.dir/Astar.o"
+  "Astar.pdb"
+  "Astar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Astar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
